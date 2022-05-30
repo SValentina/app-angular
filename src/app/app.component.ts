55b99147ab6601ccba6dcbf7780b107294fdd39b
SValentina <39774399+SValentina@users.noreply.github.com>
2022-05-30 10:25:38 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment.jenkins';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   input: string = '';
   result: string = '';
-
+  title: string = environment.title;
 
   pressNum(num: string) {
 
